83b6b67b9988a444d74aebd3607eb6f8aecd95fb
Jenny <jenny.m.lewis@hotmail.co.uk>
2021-12-06 16:15:09 +0000

    Task 2 c complete

--- index.js
+++ index.js
@@ -18,7 +18,14 @@ let myCollection = [
   console.log (myCollection);
 
 function describeItem(item) {
-    console.log (item.count)
+  if (item.count === 1) {
+    console.log("I have a " + item.name + ". Here's what I like about it: " + item.whatILike);
+  } else {
+    console.log("I have " + item.count + item.name + "s. Here's what I like about them: " + item.whatILike);
+  }
+  //if function, if one item console log "i have a item.name. Here's what I item.whatILike"
+  //else console log "I have item.count item.names. Here's what I like about them: item.whatIlike"
+    //console.log (item.count)
 }
 
 describeItem (myCollection[0]); 
